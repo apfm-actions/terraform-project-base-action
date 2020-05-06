3a3c31a0dbe17dcda8077e9f3bc573f222de569d
Mark Ferrell <mark.ferrell@aplaceformom.com>
2020-05-06 12:17:08 -0700

    fix: specify remote state workspace

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@
 # Find our vpc_id and the like via the comman services remote_state s3 key.
 data "terraform_remote_state" "shared_infra" {
   backend   = "s3"
+  workspace = terraform.workspace
 
   config = {
     region = var.region
