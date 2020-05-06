4aa14ed870d90a09417c716edc3f3d8c5d0bbd8d
Mark Ferrell <mark.ferrell@aplaceformom.com>
2020-05-06 11:56:05 -0700

    fix: specify AWS region of remote data store

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ data "terraform_remote_state" "shared_infra" {
   backend   = "s3"
 
   config = {
+    region = var.region
     bucket = var.remote_state_bucket
     key    = var.shared_state_key
   }
