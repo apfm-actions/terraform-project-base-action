797185f9b531bfc6ef01751586b25961e2c4cf94
Mark Ferrell <mark.ferrell@aplaceformom.com>
2020-05-14 07:45:57 -0700

    feat: define Terraform inputs

---
+++ inputs.tf
@@ -0,0 +1,27 @@
+variable "project" {
+  type = string
+}
+
+variable "owner" {
+  type = string
+}
+
+variable "email" {
+  type = string
+}
+
+variable "remote_state_bucket" {
+  type = string
+}
+
+variable "remote_state_table" {
+  type = string
+}
+
+variable "remote_lock_table" {
+  type = string
+}
+
+variable "shared_state_key" {
+  type = string
+}
